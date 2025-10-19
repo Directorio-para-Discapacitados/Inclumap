c0d350c8d653e7b4d0bf9e39f48e4e4524eb5a36
Jhair7474 <marlonmoncayo7474@gmail.com>
2025-10-18 21:28:00 -0500

    Creacion de Module-Service-Controlador y Entity

---
+++ backend/src/user/entity/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({ name: 'users'})
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    user_id : number;
+
+    @Column({ type: 'varchar', length: 255,  unique: true })
+    user_email: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    user_password: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    registration_date: string;
+
+}
