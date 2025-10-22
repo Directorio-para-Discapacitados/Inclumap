064684f2f93b2daa98461cd111fd182cfde92c8e
Jhair7474 <marlonmoncayo7474@gmail.com>
2025-10-22 18:19:35 -0500

    Se elimina archivos de prueba y entorno innecesarios

--- backend/src/config/roles.constants.ts
+++
@@ -1,2 +0,0 @@
-export const ALLOWED_ROLES = ['usuario', 'administrador'] as const;
-export type AllowedRole = (typeof ALLOWED_ROLES)[number];
--- backend/src/roles/dtos/create-role.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsIn, IsString } from 'class-validator';
-import { ALLOWED_ROLES } from '../../config/roles.constants';
-
-export class CreateRoleDto {
-  @IsString()
-  @IsIn(ALLOWED_ROLES as readonly string[])
-  role_name: string;
-}
