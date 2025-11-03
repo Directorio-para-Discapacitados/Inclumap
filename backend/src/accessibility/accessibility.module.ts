b01402fced3e9818164dfdbc9b3b60773f810c0b
Jhair7474 <marlonmoncayo7474@gmail.com>
2025-11-03 11:23:29 -0500

    Implementacion de seed para accesibilidades por defecto en la BD

--- backend/src/accessibility/accessibility.module.ts
+++ backend/src/accessibility/accessibility.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccessibilityController } from './accessibility.controller';
 import { AccessibilityService } from './accessibility.service';
 import { AccessibilityEntity } from './entity/accesibility.entity';
+import { AccessibilitySeed } from './seed/seed.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccessibilityEntity])],
   controllers: [AccessibilityController],
-  providers: [AccessibilityService],
+  providers: [AccessibilityService, AccessibilitySeed], 
   exports: [AccessibilityService],
 })
-export class AccessibilityModule {}
+export class AccessibilityModule implements OnApplicationBootstrap {
+  
+  constructor(private readonly accessibilitySeed: AccessibilitySeed) {}
+
+  async onApplicationBootstrap() {
+    await this.accessibilitySeed.seed();
+  }
+}
