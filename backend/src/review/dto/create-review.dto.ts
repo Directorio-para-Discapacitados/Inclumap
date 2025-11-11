9703c86fe5fec30707ca6bee187050683d11be24
Jhair7474 <marlonmoncayo7474@gmail.com>
2025-11-11 14:44:21 -0500

    Implementacion de endpoints en Controller y Logica en Service

--- backend/src/review/dto/create-review.dto.ts
+++ backend/src/review/dto/create-review.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
     IsInt,
     IsNotEmpty,
@@ -9,16 +10,19 @@ import {
   } from 'class-validator';
   
   export class CreateReviewDto {
+    @ApiProperty({description: 'La calificación (rating) del 1 al 5',})
     @IsNotEmpty()
     @IsInt()
     @Min(1)
     @Max(5)
     rating: number;
   
+    @ApiProperty({ description: 'El comentario de la reseña (opcional)',})
     @IsOptional()
     @IsString()
     comment?: string;
   
+    @ApiProperty({description: 'El ID del local (business) al que pertenece la reseña'})
     @IsNotEmpty()
     @IsNumber()
     business_id: number;
--- backend/src/review/dto/update-review.dto.ts
+++ backend/src/review/dto/update-review.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
     IsInt,
     IsOptional,
@@ -7,12 +8,15 @@ import {
   } from 'class-validator';
   
   export class UpdateReviewDto {
+    @ApiProperty({description: 'La nueva calificación (rating) del 1 al 5',})
     @IsOptional() 
     @IsInt()
     @Min(1)
     @Max(5)
     rating?: number; 
   
+    @ApiProperty({
+      description: 'El nuevo comentario de la reseña', example: 'Actualizo mi opinión, el servicio ha mejorado.',})
     @IsOptional() 
     @IsString()
     comment?: string; 
