208e7d358cf96558e807a20b2a8d87afa0494bb5
Jhair7474 <marlonmoncayo7474@gmail.com>
2025-11-15 00:13:18 -0500

    Corregir errores de linting y tests unitarios para pasar CI

--- backend/src/local-recognition/local-recognition.controller.spec.ts
+++ backend/src/local-recognition/local-recognition.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocalRecognitionController } from './local-recognition.controller';
+import { LocalRecognitionService } from './local-recognition.service'; // <-- 1. Importa el Servicio
 
 describe('LocalRecognitionController', () => {
   let controller: LocalRecognitionController;
 
+  // 2. Crea un mock del servicio
+  const mockLocalRecognitionService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LocalRecognitionController],
+      // 3. Provee el servicio simulado
+      providers: [
+        {
+          provide: LocalRecognitionService,
+          useValue: mockLocalRecognitionService,
+        },
+      ],
     }).compile();
 
     controller = module.get<LocalRecognitionController>(
